a6a5027e5abb4938d12e3ff58f431d433ea49201
Tina Nguyen <nguyentina2005@yahoo.com>
2018-03-26 16:53:40 -0400

    able to connect to twitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {RouterModule} from '@angular/router';
 import  {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng4TwitterTimelineModule } from 'ng4-twitter-timeline/lib/index';
+ 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -22,13 +24,14 @@ import { TwitterComponent } from './twitter/twitter.component';
     FormsModule,
     HttpModule,
     NgbModule,
+    Ng4TwitterTimelineModule.forRoot(),
     RouterModule.forRoot([
       {path: "", component: MovieComponent},
       {path: "twitter", component: TwitterComponent}
       
     ])
     ],
-  providers: [AppComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/twitter/twitter.component.ts
+++ src/app/twitter/twitter.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Ng4TwitterTimelineService } from 'ng4-twitter-timeline/lib/index';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-twitter',
   templateUrl: './twitter.component.html',
   styleUrls: ['./twitter.component.css']
 })
-export class TwitterComponent implements OnInit {
+export class TwitterComponent{
 
-  constructor() { }
+  
+  constructor(private ng4TwitterTimelineService: Ng4TwitterTimelineService) { }
 
-  ngOnInit() {
-  }
 
-}
+} 
+
